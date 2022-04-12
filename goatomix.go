982521e4417ff54259184ee7831e664c3dbf5212
Treborg <treborg@lbob.uk>
2022-04-12 11:32:47 +0000

    read a file

--- goatomix.go
+++ goatomix.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	bytes, err := os.ReadFile("levels/draknek.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(bytes))
+
 }
